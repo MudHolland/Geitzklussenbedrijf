b6aa89d390c48e722623540765c619037f41a6a6
MudHolland <103423480+MudHolland@users.noreply.github.com>
2025-02-21 14:05:39 +0100

    added sender and receiver email styling

--- assets/contact.php
+++ assets/contact.php
@@ -1,37 +1,70 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  //sender data
-  $senderName = htmlspecialchars(trim($_POST['name']));
-  $senderEmail = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
-  $senderMessage = htmlspecialchars(trim($_POST['message']));
+    // Sender data
+    $senderName = htmlspecialchars(trim($_POST['name']));
+    $senderEmail = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
+    $senderMessage = htmlspecialchars(trim($_POST['message']));
 
-  // receiver data
-  $receiverMail = "info@geitzklussenbedrijf.nl";
+    if (!$senderEmail) {
+        die("Ongeldig e-mailadres. Probeer het opnieuw.");
+    }
 
-  // email to receiver
-  $to = "$receiverMail";                           
-  $subject = "Bericht via de website van $senderName";
-  $body = "Naam: $senderName\nE-mail: $senderEmail\n\nBericht:\n$senderMessage";
-  
-  // email to sender
-  $headers = "From: $receiverMail\r\n";
-  $headers .= "Reply-To: $receiverMail\r\n";
-  $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    // Receiver data
+    $receiverMail = "info@geitzklussenbedrijf.nl";
 
-  $mailSuccess = mail($to, $subject, $body, $headers);
+    // Email to receiver
+    $to = $receiverMail;
+    $subject = "Bericht via de website van $senderName";
+    $body = "
+        <html>
+        <head><meta charset='UTF-8'></head>
+        <body>
+            <p>Dit is een kopie van een formulier dat u via <a href='https://www.geitzklussenbedrijf.nl'>www.geitzklussenbedrijf.nl</a> heeft verstuurd.</p>
+            <p><b>Naam:</b> $senderName</p>
+            <p><b>E-mailadres:</b> $senderEmail</p>
+            <p><b>Bericht:</b><br>$senderMessage</p>
+            <p>Ik streef ernaar om binnen 1 á 2 werkdagen te antwoorden.</p>
+        </body>
+        </html>
+    ";
 
-  if ($mailSuccess) {
-      // Send a copy to the sender
-      $subject_copy = "Kopie van uw bericht aan Geitz Klussenbedrijf";
-      $body_copy = "Beste $senderName,\n\nHier is een kopie van uw bericht:\n\n$senderMessage\n\nMet vriendelijke groet,\nGeitz Klussenbedrijf";
-      $headers_copy = "From: $receiverMail\r\nContent-Type: text/plain; charset=UTF-8\r\n";
-      mail($senderEmail, $subject_copy, $body_copy, $headers_copy);
+    $headers = "From: $receiverMail\r\n";
+    $headers .= "Reply-To: $senderEmail\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-      // Redirect to thank-you page
-      header("Location: /bericht-ontvangen/");
-      exit();
-  } else {
-      die("Er is een fout opgetreden bij het verzenden van uw bericht. Probeer het later opnieuw.");
-  }
+    $mailSuccess = mail($to, $subject, $body, $headers);
+
+    if ($mailSuccess) {
+        // Send a copy to the sender
+        $subject_copy = "Kopie van uw bericht aan Geitz Klussenbedrijf";
+        $body_copy = "
+        <html>
+        <head><meta charset='UTF-8'></head>
+        <body>
+            <p>Dit is een kopie van een formulier dat u via <a href='https://www.geitzklussenbedrijf.nl'>www.geitzklussenbedrijf.nl</a> heeft verstuurd.</p>
+            <p><b>Naam:</b> $senderName</p>
+            <p><b>E-mailadres:</b> $senderEmail</p>
+            <p><b>Bericht:</b><br>$senderMessage</p>
+            <p>Bedankt voor je bericht. Ik streef ernaar om binnen 1 á 2 werkdagen te antwoorden.</p>
+            <p>Met vriendelijke groet,</p>
+            <p><b>Marc Geitz</b><br>Geitz Klussenbedrijf</p>
+        </body>
+        </html>
+        ";
+
+        $headers_copy = "From: $receiverMail\r\n";
+        $headers_copy .= "Reply-To: $receiverMail\r\n";
+        $headers_copy .= "MIME-Version: 1.0\r\n";
+        $headers_copy .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+        mail($senderEmail, $subject_copy, $body_copy, $headers_copy);
+
+        // Redirect to thank-you page
+        header("Location: /bericht-ontvangen/");
+        exit();
+    } else {
+        die("Er is een fout opgetreden bij het verzenden van uw bericht. Probeer het later opnieuw.");
+    }
 }
 ?>
