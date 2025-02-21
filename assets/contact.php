b0d719285db7f074200121f7e280e0bd7f763910
MudHolland <103423480+MudHolland@users.noreply.github.com>
2025-02-21 13:34:22 +0100

    added headers to contact form

--- assets/contact.php
+++ assets/contact.php
@@ -8,24 +8,31 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     die("Ongeldig e-mailadres. Probeer het opnieuw.");
   }
 
-  $to = "dennisulijn@gmail.com";                                  // change to info@geitzklussenbedrijf.nl
+  $to = "dennisulijn@gmail.com";
   $subject = "Nieuw bericht van $name";
   $body = "Naam: $name\nE-mail: $email\n\nBericht:\n$message";
-  
-  // Set email headers for recipient
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
   $headers = "From: info@geitzklussenbedrijf.nl\r\n";
   $headers .= "Reply-To: $email\r\n";
   $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-  // Send email to the recipient
+  $to = "dennisulijn@gmail.com";
+  $subject = "Nieuw bericht van $name";
+  $body = "Naam: $name\nE-mail: $email\n\nBericht:\n$message";
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
+  // Send email and check for success
   $mailSuccess = mail($to, $subject, $body, $headers);
 
   if ($mailSuccess) {
-      // Send a copy to the sender
+      // Send copy to sender
       $subject_copy = "Kopie van uw bericht aan Geitz Klussenbedrijf";
       $body_copy = "Beste $name,\n\nHier is een kopie van uw bericht:\n\n$message\n\nMet vriendelijke groet,\nGeitz Klussenbedrijf";
-      $headers_copy = "From: info@geitzklussenbedrijf.nl\r\nContent-Type: text/plain; charset=UTF-8\r\n";
-      mail($email, $subject_copy, $body_copy, $headers_copy);
+
+      mail($email, $subject_copy, $body_copy, "From: info@geitzklussenbedrijf.nl\r\nContent-Type: text/plain; charset=UTF-8\r\n");
 
       // Redirect to thank-you page
       header("Location: /bericht-ontvangen/");
