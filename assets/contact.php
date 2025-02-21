eb6668a973cb751c74bff5323f6160be4ac7e6cd
MudHolland <103423480+MudHolland@users.noreply.github.com>
2025-02-21 14:07:37 +0100

    receiver and sender email working. Receiver email back to dev mail address

--- assets/contact.php
+++ assets/contact.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
     // Receiver data
-    $receiverMail = "info@geitzklussenbedrijf.nl";
+    $receiverMail = "dennisulijn@gmail.com";
 
     // Email to receiver
     $to = $receiverMail;
